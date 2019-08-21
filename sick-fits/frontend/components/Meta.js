ab70f09194a08dfc8439089dcaec034d4b948ec5
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-08-21 23:52:12 +0100

    ch-setup-appolo-client -sets up apollo-boost -pagination apollo store

---
+++ sick-fits/frontend/components/Meta.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta charSet="UTF-8" />
+            <link rel="shortcut icon" href="/static/favicon.png" />
+            <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+            <title>Sick fits!!</title>
+        </Head>
+    )
+}
