779c7eb8a90647e5f0458b4ad5517d77f5347bc0
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-08-24 23:35:14 +0100

    created the items component

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -7,8 +7,8 @@ const Mutations = {
     // }
     async createItem(parent, args, context, info) {
         // TOD: check if they are logged in
-        
-        const item = await context.db.mutation.createItem({
+
+        const item = await context.db.mutation.createItem({ 
             data:{...args}
         }, info);
         return item;
